c76a335a43f22f8f4a520ed340a4f051edf0cfd7
Emma Stotz <stotz.emma@gmail.com>
2026-05-11 22:42:50 -0400

    feat: add `AutoH,` use `useHeadingLevel()`

---
+++ src/components/primitives/AutoH.tsx
@@ -0,0 +1,18 @@
+import {
+  useHeadingLevel,
+  type HeadingLevel,
+  HEADING_TAGS,
+} from '../../hooks/useHeadingLevel'
+
+type Props = {
+  level?: HeadingLevel
+  className?: string
+  children: React.ReactNode
+}
+
+export const AutoH = ({ level, className, children }: Props) => {
+  const contextLevel = useHeadingLevel()
+  const Tag = HEADING_TAGS[level || contextLevel]
+
+  return <Tag className={className}>{children}</Tag>
+}

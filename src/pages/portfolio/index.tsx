dbb1feb5f8eb35203d6018da9807fa66b9cc5a66
Emma Stotz <stotz.emma@gmail.com>
2026-05-14 16:40:15 -0400

    feat: add placeholder page for `/portfolio`

---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,11 @@
+import { DefaultLayout } from '../../layouts/DefaultLayout'
+
+export default function PortfolioPage() {
+  return (
+    <>
+      <DefaultLayout>
+        <main className="relative">Hello world</main>
+      </DefaultLayout>
+    </>
+  )
+}

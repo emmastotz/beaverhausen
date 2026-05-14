6e1b10f6944f5b26fe5d2b59ec5e34dab895e460
Emma Stotz <stotz.emma@gmail.com>
2026-05-14 13:34:27 -0400

    feat: add `DefaultLayout`

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,50 @@
+// ─── Types ────────────────────────────────────────────────────────────────────
+
+type StyleProps = {
+  grid: boolean
+}
+
+type Props = {
+  children: React.ReactNode
+  className?: string
+  grid?: boolean
+}
+
+// ─── Styles ───────────────────────────────────────────────────────────────────
+
+const PageStyles = ({ grid }: StyleProps) => (
+  <style>{`
+    ${
+      grid
+        ? `
+    .page::before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      background-image:
+        linear-gradient(rgba(120, 97, 77, 0.08) 1px, transparent 1px),
+        linear-gradient(90deg, rgba(120, 97, 77, 0.08) 1px, transparent 1px);
+      background-size: 160px 160px;
+      pointer-events: none;
+    }
+    `
+        : ''
+    }
+  `}</style>
+)
+
+// ─── Component ────────────────────────────────────────────────────────────────
+
+export function DefaultLayout({
+  children,
+  className = '',
+  grid = true,
+}: Props) {
+  return (
+    <>
+      <PageStyles grid={grid} />
+
+      <div className={`page relative bg-cream ${className}`}>{children}</div>
+    </>
+  )
+}

4f769d0d71ab87f50184731b1505b2f1395842ab
Emma Stotz <stotz.emma@gmail.com>
2026-05-15 08:05:24 -0400

    refactor: prefer to use `Outlet` for cleaner separation of concerns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router'
+
+import {
+  useTransition,
+  TransitionProvider,
+} from './components/transition/TransitionContext'
+import { TransitionOverlay } from './components/transition/TransitionOverlay'
 import LandingPage from './pages/index'
+import PortfolioPage from './pages/portfolio'
+
+function RootLayout() {
+  const { state } = useTransition()
+  return (
+    <>
+      <TransitionOverlay state={state} />
+      <Outlet />
+    </>
+  )
+}
 
 const router = createBrowserRouter([
-  { path: '/', element: <LandingPage /> },
+  {
+    element: (
+      <TransitionProvider>
+        <RootLayout />
+      </TransitionProvider>
+    ),
+    children: [
+      { path: '/', element: <LandingPage /> },
+      { path: '/portfolio', element: <PortfolioPage /> },
+    ],
+  },
 ])
 
 export default function App() {

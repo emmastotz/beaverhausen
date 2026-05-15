d92aa323b6c3359bff8961476c7ef23db04b1b0d
Emma Stotz <stotz.emma@gmail.com>
2026-05-15 12:10:18 -0400

    feat: add placeholder `/about` page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import { DefaultLayout } from '../../layouts/DefaultLayout'
+
+export default function AboutPage() {
+  return (
+    <>
+      <DefaultLayout>
+        <main className="relative">Static About Page</main>
+      </DefaultLayout>
+    </>
+  )
+}

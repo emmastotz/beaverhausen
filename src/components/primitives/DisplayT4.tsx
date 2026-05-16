ecbbea198d82f9ed258e9f52ea66d4ed3274d0eb
Emma Stotz <stotz.emma@gmail.com>
2026-05-16 17:38:20 -0400

    feat: add `DisplayT4`

---
+++ src/components/primitives/DisplayT4.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children: React.ReactNode
+  className?: string
+}
+
+export function DisplayT4({ children, className = '' }: Props) {
+  return (
+    <span
+      className={`font-display text-sm tracking-[0.18em] antialiased sm:text-base ${className}`}
+    >
+      {children}
+    </span>
+  )
+}

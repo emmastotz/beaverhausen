d2be425e9a478d25c8bdfd0ccc7c070eb43701a9
Emma Stotz <stotz.emma@gmail.com>
2026-05-16 17:39:29 -0400

    feat: add `variant` prop + conditional render

--- src/components/primitives/BaseT6.tsx
+++ src/components/primitives/BaseT6.tsx
@@ -6,7 +6,7 @@ type Props = {
 export function BaseT6({ children, className = '' }: Props) {
   return (
     <span
-      className={`font-body text-sm font-normal tracking-wide antialiased sm:text-base ${className}`}
+      className={`font-body text-xs tracking-[0.18em] antialiased ${className}`}
     >
       {children}
     </span>

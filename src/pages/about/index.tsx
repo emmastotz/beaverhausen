0b4f3c6b0fc7594be9e68f362efa292ccf04d868
Emma Stotz <stotz.emma@gmail.com>
2026-05-16 17:23:36 -0400

    feat: use `<StaticAbout>`

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,12 @@
+import { StaticAbout } from '../../components/StaticAbout'
 import { DefaultLayout } from '../../layouts/DefaultLayout'
 
 export default function AboutPage() {
   return (
-    <>
-      <DefaultLayout>
-        <main className="relative">Static About Page</main>
-      </DefaultLayout>
-    </>
+    <DefaultLayout>
+      <main className="relative mx-auto max-w-2xl px-8 py-40 lg:px-0">
+        <StaticAbout />
+      </main>
+    </DefaultLayout>
   )
 }

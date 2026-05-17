f10286bc9a2a85cb13ab1f36a8721f2e2ab25afd
Emma Stotz <stotz.emma@gmail.com>
2026-05-17 11:43:22 -0400

    feat: prefer `font-medium`

--- src/components/primitives/DisplayT1.tsx
+++ src/components/primitives/DisplayT1.tsx
@@ -6,7 +6,7 @@ type Props = {
 export function DisplayT1({ children, className = '' }: Props) {
   return (
     <span
-      className={`font-display text-xl font-semibold tracking-wide antialiased sm:text-2xl ${className}`}
+      className={`font-display text-xl font-medium tracking-wide antialiased sm:text-2xl ${className}`}
     >
       {children}
     </span>

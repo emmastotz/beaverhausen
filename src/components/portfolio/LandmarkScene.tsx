47fdb1ad85200a66ff8b97df96cf586684b4afea
Emma Stotz <stotz.emma@gmail.com>
2026-05-21 18:57:30 -0400

    feat: add ArrowCue

--- src/components/portfolio/LandmarkScene.tsx
+++ src/components/portfolio/LandmarkScene.tsx
@@ -11,6 +11,31 @@ interface Props {
   scene: SceneConfig
 }
 
+function ArrowCue({ visible }: { visible: boolean }) {
+  return (
+    <div
+      className={`absolute flex flex-col items-center gap-0.5 transition-all duration-200 ${visible ? 'translate-y-0 opacity-100' : '-translate-y-1 opacity-0'} `}
+    >
+      <BaseT6 className="mb-2 text-iron-orange uppercase">Hover</BaseT6>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1"
+        stroke="currentColor"
+        className="size-6 animate-bounce text-iron-orange"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3"
+        />
+      </svg>
+    </div>
+  )
+}
+
 export function LandmarkScene({ scene }: Props) {
   const [hovered, setHovered] = useState(false)
   const { transitionTo } = useTransition()
@@ -49,7 +74,7 @@ export function LandmarkScene({ scene }: Props) {
 
       {/* Scene info */}
       <div
-        className={`pointer-events-none absolute bottom-full mb-60 text-center transition-all duration-300 ease-out ${
+        className={`pointer-events-auto absolute bottom-full mb-60 text-center transition-all duration-300 ease-out ${
           hovered ? 'translate-y-0 opacity-100' : 'translate-y-2 opacity-0'
         } `}
         style={{ left, width }}
@@ -72,6 +97,13 @@ export function LandmarkScene({ scene }: Props) {
             {scene.role}
           </BaseT6>
         </p>
+      </div>
+
+      <div
+        className="absolute flex items-center justify-center"
+        style={{ bottom: `calc(${bottom} + 100% + 2rem)`, left, width }}
+      >
+        <ArrowCue visible={!hovered} />
       </div>
     </div>
   )

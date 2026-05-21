38fe6d372aa5991ac10eea50eea198ee9900a4d1
Emma Stotz <stotz.emma@gmail.com>
2026-05-21 18:05:02 -0400

    feat: add orientation text at beginning

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,4 +1,8 @@
 import { PortfolioParallax } from '../../components/portfolio/PortfolioParallax'
+import { AutoH } from '../../components/primitives/AutoH'
+import { BaseT1 } from '../../components/primitives/BaseT1'
+import { BaseT3 } from '../../components/primitives/BaseT3'
+import { BaseT6 } from '../../components/primitives/BaseT6'
 import { DefaultLayout } from '../../layouts/DefaultLayout'
 
 export default function PortfolioPage() {
@@ -6,8 +10,30 @@ export default function PortfolioPage() {
     <>
       <DefaultLayout>
         <main className="relative">
-          <PortfolioParallax />
+          <div className="absolute inset-0 top-60">
+            <div className="flex flex-col gap-8 text-center">
+              <div>
+                <BaseT6 className="text-iron-orange uppercase">
+                  Selected works
+                </BaseT6>
+                <AutoH>
+                  <BaseT1 className="text-beaver-dark">
+                    Something is always being built
+                  </BaseT1>
+                </AutoH>
+              </div>
+
+              <p className="mx-auto max-w-lg">
+                <BaseT3 className="text-beaver">
+                  Click on a site for a more in-depth exploration. Hover for the
+                  details.
+                </BaseT3>
+              </p>
+            </div>
+          </div>
         </main>
+
+        <PortfolioParallax />
       </DefaultLayout>
     </>
   )

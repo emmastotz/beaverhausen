fa2efb20330743838cb234a145555a0e213f0a50
Emma Stotz <stotz.emma@gmail.com>
2026-05-26 12:04:19 -0400

    feat: prefer `@` alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -9,6 +10,11 @@ export default defineConfig({
     port: 3000,
   },
   base: '/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),

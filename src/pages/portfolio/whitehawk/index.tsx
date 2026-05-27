4a5bf6781b0d8a38264639fed8708e5a9218adc4
Emma Stotz <stotz.emma@gmail.com>
2026-05-27 19:04:06 -0400

    feat: container sizing on parent container

--- src/pages/portfolio/whitehawk/index.tsx
+++ src/pages/portfolio/whitehawk/index.tsx
@@ -1,11 +1,13 @@
+import { CaseStudyNav } from '@/components/portfolio/CaseStudyNav'
 import { WhiteHawkCaseStudy } from '@/components/portfolio/whitehawk/WhiteHawkCaseStudy'
 import { DefaultLayout } from '@/layouts/DefaultLayout'
 
 export default function WhiteHawkPortfolioPage() {
   return (
     <DefaultLayout>
-      <main className="relative overflow-x-hidden">
+      <main className="relative mx-auto min-h-screen max-w-7xl overflow-x-hidden">
         <WhiteHawkCaseStudy />
+        <CaseStudyNav currentHref="/portfolio/whitehawk" />
       </main>
     </DefaultLayout>
   )

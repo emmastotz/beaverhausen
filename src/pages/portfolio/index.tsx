987635bc19c54bf8eebf52dc3ee0ef2fa5bdd637
Emma Stotz <stotz.emma@gmail.com>
2026-05-26 21:24:28 -0400

    feat: hide portfolio text when user starts to scroll

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import { PortfolioParallax } from '@/components/portfolio/PortfolioParallax'
 import { AutoH } from '@/components/primitives/AutoH'
 import { BaseT1 } from '@/components/primitives/BaseT1'
@@ -6,13 +8,23 @@ import { BaseT6 } from '@/components/primitives/BaseT6'
 import { DefaultLayout } from '@/layouts/DefaultLayout'
 
 export default function PortfolioPage() {
+  const [heroVisible, setHeroVisible] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => setHeroVisible(window.scrollY < 100)
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <>
       <DefaultLayout>
-        <main className="relative">
-          <div className="absolute inset-0 top-60">
-            <div className="flex flex-col gap-8 text-center">
-              <div>
+        <main className="relative mx-4 md:mx-12 lg:mx-0">
+          <div
+            className={`absolute inset-0 top-30 transition-opacity duration-300 lg:top-50 ${heroVisible ? 'opacity-100' : 'pointer-events-none opacity-0'}`}
+          >
+            <div className="flex flex-col gap-4 text-center sm:gap-8">
+              <div className="mx-auto max-w-md lg:max-w-none">
                 <BaseT6 className="text-iron-orange uppercase">
                   Selected works
                 </BaseT6>
@@ -24,16 +36,20 @@ export default function PortfolioPage() {
               </div>
 
               <p className="mx-auto max-w-lg">
-                <BaseT3 className="text-beaver">
-                  Click on a site for a more in-depth exploration. Hover for the
-                  details.
+                <BaseT3 className="hidden text-beaver lg:block">
+                  Click on a site for a more in-depth exploration.
+                </BaseT3>
+                <BaseT3 className="text-beaver lg:hidden">
+                  Tap on a site for a more in-depth exploration.
                 </BaseT3>
               </p>
             </div>
           </div>
         </main>
 
-        <PortfolioParallax />
+        <div onFocus={() => setHeroVisible(false)}>
+          <PortfolioParallax />
+        </div>
       </DefaultLayout>
     </>
   )

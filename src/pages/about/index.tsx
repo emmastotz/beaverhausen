ff1701ec9c7b4d1738be57ac5e93ac08f121a53e
Emma Stotz <stotz.emma@gmail.com>
2026-05-26 22:33:06 -0400

    feat: consistent padding

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,7 +4,7 @@ import { DefaultLayout } from '@/layouts/DefaultLayout'
 export default function AboutPage() {
   return (
     <DefaultLayout>
-      <main className="relative mx-auto max-w-2xl px-8 py-40 lg:px-0">
+      <main className="relative mx-auto max-w-2xl px-8 pt-20 pb-14 sm:pt-24 lg:px-0">
         <StaticAbout />
       </main>
     </DefaultLayout>

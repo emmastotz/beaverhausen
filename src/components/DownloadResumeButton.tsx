49c1c05d409adc3d0fe66bac0a28c58a340ecc31
Emma Stotz <stotz.emma@gmail.com>
2026-05-28 09:31:06 -0400

    feat: add `DownloadResumeButton`

---
+++ src/components/DownloadResumeButton.tsx
@@ -0,0 +1,29 @@
+import type { Variant, Size } from '@/components/primitives/interactiveStyles'
+
+import { Button } from '@/components/primitives/Button'
+
+interface Props {
+  variant?: Variant
+  size?: Size
+}
+
+export function DownloadResumeButton({ variant, size }: Props) {
+  const handleDownload = async () => {
+    const response = await fetch(
+      'https://beaverhausen-worker.beaverhausen.workers.dev/resume',
+    )
+    const blob = await response.blob()
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'Emma-Stotz-Resume-2026.pdf'
+    a.click()
+    URL.revokeObjectURL(url)
+  }
+
+  return (
+    <Button variant={variant} size={size} onClick={handleDownload}>
+      Download resume
+    </Button>
+  )
+}

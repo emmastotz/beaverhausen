f6b0d35d7832980e410a8194470c1f2a60e89a5a
Emma Stotz <stotz.emma@gmail.com>
2026-06-01 22:41:41 -0400

    feat: use `underline-build` animation token

--- src/components/primitives/interactiveStyles.ts
+++ src/components/primitives/interactiveStyles.ts
@@ -16,10 +16,7 @@ export const baseInteractiveClasses = [
   'relative inline-flex items-center justify-center',
   'bg-transparent cursor-pointer outline-none',
   'font-body tracking-[0.18em] uppercase transition-colors duration-200',
-  "before:content-[''] before:absolute before:bottom-0 before:h-[2px] before:w-0 before:bg-iron-orange before:right-1/2",
-  'before:[transition:width_var(--duration-interaction)_var(--ease-out-cubic)] hover:before:w-1/2 focus-visible:before:w-1/2 focus-visible:before:[transition:none]',
-  "after:content-[''] after:absolute after:bottom-0 after:h-[2px] after:w-0 after:bg-iron-orange after:left-1/2",
-  'after:[transition:width_var(--duration-interaction)_var(--ease-out-cubic)] hover:after:w-1/2 focus-visible:after:w-1/2 focus-visible:after:[transition:none]',
+  'underline-build',
 ].join(' ')
 
 export function buildInteractiveClass(

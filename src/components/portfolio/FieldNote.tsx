efacf5ec387bbbc9e87752a8f73a14222fcad371
Emma Stotz <stotz.emma@gmail.com>
2026-06-01 20:11:20 -0400

    feat: add `FieldNote` component

---
+++ src/components/portfolio/FieldNote.tsx
@@ -0,0 +1,13 @@
+import { BaseT5 } from '@/components/primitives/BaseT5'
+
+export function FieldNote({ text }: { text: string }) {
+  return (
+    <div className="mt-2 mb-8 border-l-2 border-iron-orange pl-4">
+      <p>
+        <BaseT5 variant="display" className="tracking-normal text-beaver italic">
+          <strong className="tracking-wide">Field note:</strong> {text}
+        </BaseT5>
+      </p>
+    </div>
+  )
+}

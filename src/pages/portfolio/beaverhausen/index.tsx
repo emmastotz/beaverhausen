1429b999dc42bfe2e350c66e04ccbbc2a861846e
Emma Stotz <stotz.emma@gmail.com>
2026-06-03 14:00:03 -0400

    feat: use `CaseStudyLayout`

--- src/pages/portfolio/beaverhausen/index.tsx
+++ src/pages/portfolio/beaverhausen/index.tsx
@@ -1,14 +1,14 @@
 import { BeaverhausenCaseStudy } from '@/components/portfolio/case-studies/beaverhausen/BeaverhausenCaseStudy'
 import { CaseStudyNav } from '@/components/portfolio/case-studies/CaseStudyNav'
-import { DefaultLayout } from '@/layouts/DefaultLayout'
+import { CaseStudyLayout } from '@/layouts/CaseStudyLayout'
 
 export default function BeaverhausenPortfolioPage() {
   return (
-    <DefaultLayout>
+    <CaseStudyLayout>
       <main className="mx-auto min-h-screen max-w-7xl">
         <BeaverhausenCaseStudy />
         <CaseStudyNav currentHref="/portfolio/beaverhausen" />
       </main>
-    </DefaultLayout>
+    </CaseStudyLayout>
   )
 }

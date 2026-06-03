50abf3f1cfee1d5e645f73a710a1caeb206946b4
Emma Stotz <stotz.emma@gmail.com>
2026-06-03 14:01:02 -0400

    feat: move `Header` to `DefaultLayout`

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,26 +1,22 @@
+import { Header } from '@/components/Header'
+
 type Props = {
   children: React.ReactNode
   className?: string
-  grid?: boolean
 }
 
-export function DefaultLayout({
-  children,
-  className = '',
-  grid = true,
-}: Props) {
+export function DefaultLayout({ children, className = '' }: Props) {
   return (
-    <div className="relative bg-cream">
-      {grid && (
-        <div
-          className="pointer-events-none fixed top-16 right-0 bottom-0 left-0"
-          style={{
-            backgroundImage:
-              'linear-gradient(rgba(120, 97, 77, 0.08) 1px, transparent 1px), linear-gradient(90deg, rgba(120, 97, 77, 0.08) 1px, transparent 1px)',
-            backgroundSize: '160px 160px',
-          }}
-        />
-      )}
+    <div className="relative bg-cream dark:bg-dusk">
+      <div className="fixed inset-0">
+        <div className="fixed inset-x-0 top-0 h-1/2 bg-linear-to-b from-water-dark from-8% via-15% to-transparent dark:from-transparent dark:from-12% dark:via-water-dark dark:via-50% dark:to-water" />
+        <div className="fixed inset-x-0 bottom-0 h-1/2 bg-linear-to-t from-iron-orange from-8% via-dawn via-20% to-transparent not-dark:to-75% dark:from-beaver-dark dark:from-5% dark:via-iron-orange dark:via-18% dark:to-water" />
+      </div>
+
+      <div className="bg-beaver-tail-grid pointer-events-none fixed top-16 right-0 bottom-0 left-0" />
+
+      <Header />
+
       <div className={`relative ${className}`}>{children}</div>
     </div>
   )

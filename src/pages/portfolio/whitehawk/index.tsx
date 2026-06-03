00f0160058f941888978becf47bfb8f9250025bb
Emma Stotz <stotz.emma@gmail.com>
2026-06-03 14:00:09 -0400

    feat: use `CaseStudyLayout`

--- src/pages/portfolio/whitehawk/index.tsx
+++ src/pages/portfolio/whitehawk/index.tsx
@@ -1,14 +1,14 @@
 import { CaseStudyNav } from '@/components/portfolio/case-studies/CaseStudyNav'
 import { WhiteHawkCaseStudy } from '@/components/portfolio/case-studies/whitehawk/WhiteHawkCaseStudy'
-import { DefaultLayout } from '@/layouts/DefaultLayout'
+import { CaseStudyLayout } from '@/layouts/CaseStudyLayout'
 
 export default function WhiteHawkPortfolioPage() {
   return (
-    <DefaultLayout>
+    <CaseStudyLayout>
       <main className="mx-auto min-h-screen max-w-7xl">
         <WhiteHawkCaseStudy />
         <CaseStudyNav currentHref="/portfolio/whitehawk" />
       </main>
-    </DefaultLayout>
+    </CaseStudyLayout>
   )
 }

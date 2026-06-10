d9fa57dd1a029f0e0d0ec998d2a990990c06afbb
Emma Stotz <stotz.emma@gmail.com>
2026-06-10 16:42:33 -0400

    feat: support dark mode

--- src/content/hero.ts
+++ src/content/hero.ts
@@ -6,10 +6,10 @@ type DefinitionPart = {
 export const HERO_COPY: DefinitionPart[] = [
   {
     text: 'The dwelling of an assiduous creature suspended somewhere between design instinct and engineering precision;',
-    className: 'text-beaver-dark',
+    className: 'text-beaver-dark dark:text-cream',
   },
   {
     text: ' ecosystem modifications considered routine maintenance.',
-    className: 'italic text-beaver',
+    className: 'italic text-beaver dark:text-beaver-dark',
   },
 ]

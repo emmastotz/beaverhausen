bddf3c779967ae26be560e6392db6619caf9bf7d
Emma Stotz <stotz.emma@gmail.com>
2026-06-11 16:12:14 -0400

    feat: better responsiveness for mobile

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -4,7 +4,7 @@ import { FadeIn } from '@/components/FadeIn'
 import { PortfolioParallax } from '@/components/portfolio/PortfolioParallax'
 import { AutoH } from '@/components/primitives/AutoH'
 import { BaseT1 } from '@/components/primitives/BaseT1'
-import { BaseT3 } from '@/components/primitives/BaseT3'
+import { BaseT4 } from '@/components/primitives/BaseT4'
 import { BaseT6 } from '@/components/primitives/BaseT6'
 import { DefaultLayout } from '@/layouts/DefaultLayout'
 
@@ -20,11 +20,14 @@ export default function PortfolioPage() {
   return (
     <DefaultLayout>
       <main>
-        <div className="relative mx-4 md:mx-12 lg:mx-0">
+        <div className="z-floating relative">
           <div
-            className={`pointer-events-none absolute inset-0 top-30 transition-opacity duration-300 lg:top-50 ${heroVisible ? 'opacity-100' : 'opacity-0'}`}
+            className={`pointer-events-none absolute inset-0 top-[17svh] transition-opacity duration-300 sm:top-[18svh] md:top-[20svh] ${heroVisible ? 'opacity-100' : 'opacity-0'}`}
           >
-            <FadeIn delay={500} className="flex flex-col gap-4 text-center">
+            <FadeIn
+              delay={500}
+              className="mx-4 flex h-full flex-1 flex-col items-center justify-evenly gap-4 text-center sm:gap-0 md:mx-12 md:gap-8 lg:mx-0"
+            >
               <div className="mx-auto max-w-md lg:max-w-none">
                 <BaseT6 className="text-iron-orange uppercase dark:text-iron-orange">
                   Selected works
@@ -36,11 +39,11 @@ export default function PortfolioPage() {
                 </AutoH>
               </div>
 
-              <p className="mx-auto mt-4 max-w-lg sm:mt-8">
-                <BaseT3 className="text-beaver dark:text-beaver-dark">
+              <p className="mx-auto max-w-lg sm:max-w-none md:max-w-md">
+                <BaseT4 className="text-beaver dark:text-beaver-dark">
                   Scroll to continue the journey. Click on a site for a more
                   in-depth exploration.
-                </BaseT3>
+                </BaseT4>
               </p>
             </FadeIn>
           </div>

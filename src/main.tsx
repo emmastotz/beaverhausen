cb46ac2d731ef2cc37fa74c52b9896faface22ca
Emma Stotz <stotz.emma@gmail.com>
2026-06-11 09:22:57 -0400

    fix: conditionally import polyfill only if browser does not support animation timeline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import 'scroll-timeline-polyfill/dist/scroll-timeline.js'
 import './index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from '@/App'
+
+if (!CSS.supports('animation-timeline: scroll()')) {
+  await import('scroll-timeline-polyfill/dist/scroll-timeline.js')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

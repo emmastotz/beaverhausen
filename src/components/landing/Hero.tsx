8dcb64c1e08649760a0f367b8fd817d4c76d64e6
Emma Stotz <stotz.emma@gmail.com>
2026-06-10 22:29:02 -0400

    feat: add `will-change-*` to avoid loading issues

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -69,7 +69,7 @@ export const Hero = () => {
         <div className="flex w-full flex-col gap-4 sm:gap-2 lg:gap-8">
           <div className="flex w-full flex-col gap-2 sm:gap-1 lg:gap-2">
             <div
-              className={`transition-all duration-800 ease-in ${
+              className={`will-change-[transform,opacity] transition-all duration-800 ease-in ${
                 showWordmark
                   ? 'translate-y-0 opacity-100'
                   : '-translate-y-3 opacity-0'
@@ -80,7 +80,7 @@ export const Hero = () => {
 
             <p
               className={[
-                'transition-all duration-800 ease-linear delay-500',
+                'will-change-[opacity] transition-all duration-800 ease-linear delay-500',
                 showWordmark ? 'opacity-100' : 'opacity-0',
               ].join(' ')}
             >
@@ -92,7 +92,7 @@ export const Hero = () => {
 
           <div
             className={[
-              'transition-opacity duration-800 ease-linear delay-700',
+              'will-change-[opacity] transition-opacity duration-800 ease-linear delay-700',
               showWordmark ? 'opacity-100' : 'opacity-0',
             ].join(' ')}
           >

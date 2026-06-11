e1b129df3ea3931ed8fe80adb316f5a9c3cb6866
Emma Stotz <stotz.emma@gmail.com>
2026-06-10 20:24:13 -0400

    feat: pass text sizes and line height

--- src/content/hero.ts
+++ src/content/hero.ts
@@ -6,10 +6,12 @@ type DefinitionPart = {
 export const HERO_COPY: DefinitionPart[] = [
   {
     text: 'The dwelling of an assiduous creature suspended somewhere between design instinct and engineering precision;',
-    className: 'text-beaver-dark dark:text-cream',
+    className:
+      'text-beaver-dark dark:text-cream text-base/6 sm:leading-none md:text-base/[1.375] lg:text-xl/[1.75]',
   },
   {
     text: ' ecosystem modifications considered routine maintenance.',
-    className: 'italic text-beaver dark:text-beaver-dark',
+    className:
+      'italic text-beaver dark:text-beaver-dark text-base/6 sm:leading-none md:text-base/[1.375] lg:text-xl/[1.75]',
   },
 ]
